39ef3c571b46b6f93bb26fbef5d6d3c7bebd0376
Dima Lunich <lunich@ukr.net>
2010-10-25 22:26:17 +0300

    columns method added
    readme and specs updated

--- lib/table_for.rb
+++ lib/table_for.rb
@@ -25,12 +25,9 @@ module ActionView
       end
     private
       class Column
-        attr_reader :view, :options, :title, :attr
-        
+        attr_reader :title
         def initialize(view, obj, ops)
-          @view = view
-          @options = ops
-          @attr = obj
+          @view, @options, @attr, @title = view, ops, obj, ""
         end
         
         def content_for
@@ -44,7 +41,7 @@ module ActionView
           @title = @options.delete(:title) || @attr.to_s.humanize || "&nbsp;"
         end
         def content_for(record)
-          record.send(self.attr)
+          record.send(@attr)
         end
       end
       
@@ -54,13 +51,11 @@ module ActionView
           @title = @options.delete(:title) || "&nbsp;"
         end
         def content_for(record)
-          attr.call(record)
+          @attr.call(record)
         end
       end
     
       class Table
-        attr_reader :options, :view, :columns, :records
-
         def initialize(view, recs, opts)
           @view = view
           @options = opts
@@ -68,14 +63,24 @@ module ActionView
           @records = recs
         end
         
+        def columns(*args)
+          unless args.blank?
+            args.each do |arg|
+              self.column(arg)
+            end
+          else
+            raise ArgumentError, "At least one attribute name should be given"
+          end
+        end
+        
         def column(*args, &block)
           col_options = args.extract_options!
           unless args.blank?
             attr = args.shift
-            self.columns << SimpleColumn.new(self.view, attr, col_options)
+            @columns << SimpleColumn.new(@view, attr, col_options)
           else
             if block_given?
-              self.columns << CallbackColumn.new(self.view, block, col_options)
+              @columns << CallbackColumn.new(@view, block, col_options)
             else
               raise ArgumentError, "Attribute name or block should be given"
             end
@@ -83,20 +88,20 @@ module ActionView
         end
 
         def draw
-          self.view.content_tag :table, self.options.delete(:html) do
+          @view.content_tag :table, @options.delete(:html) do
             head + body
           end
         end
       protected
         def method_missing(method, *args, &proc)
-          self.view.send(method, *args, &proc)
+          @view.send(method, *args, &proc)
         end
       private
         def head
-          self.view.content_tag :thead do
-            self.view.content_tag :tr do
-              self.columns.map do |col|
-                self.view.content_tag :th do
+          @view.content_tag :thead do
+            @view.content_tag :tr do
+              @columns.map do |col|
+                @view.content_tag :th do
                   col.title
                 end
               end.join
@@ -105,11 +110,11 @@ module ActionView
         end
         
         def body
-          self.view.content_tag :tbody do
-            self.records.map do |rec|
-              self.view.content_tag :tr do
-                self.columns.map do |col|
-                  self.view.content_tag :td do
+          @view.content_tag :tbody do
+            @records.map do |rec|
+              @view.content_tag :tr do
+                @columns.map do |col|
+                  @view.content_tag :td do
                     col.content_for(rec)
                   end
                 end.join
--- spec/table_for_spec.rb
+++ spec/table_for_spec.rb
@@ -1,88 +1,35 @@
 require "spec_helper"
 
 describe ActionView::Base do
+  # ActionView::Base instance
   let(:view) do
     ActionView::Base.new
   end
-  
+  # users list (stubbed data)
   let(:users) do
     [
       mock({
-        :id => 21,
         :name => "John Smith",
         :email => "tester@example.com",
         :address => "100, Spear Street, NY, USA",
       })
     ]
   end
-  
+  # check if method available
   it "should respond to :table_for" do
     view.should respond_to(:table_for)
   end
-
+  # main method
   describe ":table_for method" do
+    # <%= table_for @users %>
     it "should raise if no block given" do
       lambda do
         view.table_for(users)
       end.should raise_error(ArgumentError)
     end
-    describe "with callback column" do
-      before(:each) do
-        @html = view.table_for(users) do
-          column :title => "Addr" do |user|
-            user.address[0,10]
-          end
-        end
-      end
-      it "should render valid HTML" do
-        @html.should have_selector("table") do |table|
-          table.should have_selector("thead") do |thead|
-            thead.should have_selector("tr") do |tr|
-              tr.should have_selector("th") do |th|
-                th.should contain("Addr")
-              end
-            end
-          end
-          table.should have_selector("tbody") do |tbody|
-            tbody.should have_selector("tr") do |tr|
-              users.each do |user|
-                tr.should have_selector("td") do |td|
-                  td.should contain(user.address[0,10])
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-    
-    describe "with titled column" do
-      before(:each) do
-        @html = view.table_for(users) do
-          column :email, :title => "Email address"
-        end
-      end
-      it "should render valid HTML" do
-        @html.should have_selector("table") do |table|
-          table.should have_selector("thead") do |thead|
-            thead.should have_selector("tr") do |tr|
-              tr.should have_selector("th") do |th|
-                th.should contain("Email address")
-              end
-            end
-          end
-          table.should have_selector("tbody") do |tbody|
-            tbody.should have_selector("tr") do |tr|
-              users.each do |user|
-                tr.should have_selector("td") do |td|
-                  td.should contain(user.email)
-                end
-              end
-            end
-          end
-        end
-      end
-    end
+    # <%= table_for @users do %>
+    #   <% column :name %>
+    # <% end %>
     describe "with simple column" do
       before(:each) do
         @html = view.table_for(users) do
@@ -91,19 +38,104 @@ describe ActionView::Base do
       end
       it "should render valid HTML" do
         @html.should have_selector("table") do |table|
-          table.should have_selector("thead") do |thead|
-            thead.should have_selector("tr") do |tr|
-              tr.should have_selector("th") do |th|
-                th.should contain("Name")
+          table.should have_selector("thead/tr/th") do |th|
+            th.should contain("Name")
+          end
+          table.should have_selector("tbody/tr") do |tr|
+            users.each do |user|
+              tr.should have_selector("td") do |td|
+                td.should contain(user.name)
               end
             end
           end
-          table.should have_selector("tbody") do |tbody|
-            tbody.should have_selector("tr") do |tr|
-              users.each do |user|
-                tr.should have_selector("td") do |td|
-                  td.should contain(user.name)
-                end
+        end
+      end
+    end
+    # <%= table_for @users do %>
+    #   <% columns :name, :email, :address %>
+    # <% end %>
+    describe "with columns" do
+      before(:each) do
+        @html = view.table_for(users) do
+          columns :name, :email, :address
+        end
+      end
+      it "should render valid HTML" do
+        @html.should have_selector("table") do |table|
+          table.should have_selector("thead/tr") do |tr|
+            ["Name", "Email", "Address"].each do |field|
+              tr.should have_selector("th") do |th|
+                th.should contain(field)
+              end
+            end
+          end
+        end
+      end
+    end
+    
+    # <%= table_for @users, :html => { :id => "users", :class => "simple-table" } do %>
+    #   <% column :name %>
+    # <% end %>
+    describe "with given :html options" do
+      before(:each) do
+        @html = view.table_for(users, :html => { :id => "users", :class => "simple-table" }) do
+          column :name
+        end
+      end
+      it "should render specialized table" do
+        @html.should have_selector("table#users.simple-table")
+      end
+    end
+    # <%= table_for @users do %>
+    #   <% column :name, :title => "User's name" %>
+    # <% end %>
+    describe "with titled column" do
+      before(:each) do
+        @html = view.table_for(users) do
+          column :email, :title => "Email address"
+        end
+      end
+      it "should render valid HTML" do
+        @html.should have_selector("table") do |table|
+          table.should have_selector("thead/tr/th") do |th|
+            th.should contain("Email address")
+          end
+          table.should have_selector("tbody/tr") do |tr|
+            users.each do |user|
+              tr.should have_selector("td") do |td|
+                td.should contain(user.email)
+              end
+            end
+          end
+        end
+      end
+    end
+    # <%= table_for @users do %>
+    #   <% column :title => "User's name" do |user| %>
+    #     <% content_tag :div do %>
+    #       <% [user.first_name, user.last_name].join(" ") %>
+    #     <% end %>
+    #   <% end %>
+    # <% end %>
+    describe "with callback column" do
+      before(:each) do
+        @html = view.table_for(users) do
+          column :title => "Addr" do |user|
+            content_tag :div do
+              user.address[0,10]
+            end
+          end
+        end
+      end
+      it "should render valid HTML" do
+        @html.should have_selector("table") do |table|
+          table.should have_selector("thead/tr/th") do |th|
+            th.should contain("Addr")
+          end
+          table.should have_selector("tbody/tr") do |tr|
+            users.each do |user|
+              tr.should have_selector("td/div") do |td|
+                td.should contain(user.address[0,10])
               end
             end
           end
